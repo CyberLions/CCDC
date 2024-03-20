6e48c89ede6e7eac2dbd878ae4291c602435e342
Cy Fisher <sheriffCy@gmail.com>
2024-03-20 19:20:03 -0400

    Update brick.ps1

--- 2024/brick-scripts/brick.ps1
+++ 2024/brick-scripts/brick.ps1
@@ -1,2 +1,2 @@
-New-NetFirewallRule -DisplayName "Block All Traffic" -Direction Inbound,Outbound -Action Block
-
+New-NetFirewallRule -DisplayName "Block All Inbound Traffic" -Direction Inbound -Action Block
+New-NetFirewallRule -DisplayName "Block All Outbound Traffic" -Direction Outbound -Action Block
