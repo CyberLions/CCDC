9f6760183f61d604f1ec84c95a5a36579b5c9ad6
Cy Fisher <sheriffCy@gmail.com>
2024-03-20 20:12:00 -0400

    Update unbrick.bat

--- 2024/brick-scripts/unbrick.bat
+++ 2024/brick-scripts/unbrick.bat
@@ -1,4 +1,4 @@
 @echo off
-Rem These must match the names of the rules that blocked all traffic
+rem These must match the names of the rules that blocked all traffic
 netsh advfirewall firewall delete rule name="Block All Inbound"
 netsh advfirewall firewall delete rule name="Block All Outbound"
