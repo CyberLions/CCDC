7836b4020ed7b0cd1f0ed7b7c40257b0eb91e0c8
Hackerfox8100 <40575728+Hackerfox8100@users.noreply.github.com>
2024-04-09 15:31:30 -0400

    Add to script

---
+++ wildcard/linux/Ubuntu/initialScript.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#Check if sudo
+if [ $UID != 0 ]; then
+    echo " use sudo and try again... "
+    exit
+fi
+#Clean packages
+echo "Cleaning packages"
+apt-get update
+apt-get autoremove -y
+apt-get autoclean -y
+apt-get update
+
+read -p "Do you want to update packages? [y,n]" runupdates
+if [ $runupdates == "y" ]
+	then
+		echo "UPDATING PACKAGES"
+		apt-get upgrade -y
+fi
+#Automatic updates
+apt-get install -y unattended-upgrades
+dpkg-reconfigure unattended-upgrades
+
+#pam dependencies
+apt-get install -y libpam-cracklib
+
+
+cp ./common-password /etc/pam.d/common-password
+cp ./login.defs /etc/
+apt-get install -y gufw
+ufw enable
+sysctl -n net.ipv4.tcp_syncookies
+echo 'net.ipv6.conf.all.disable_ipv6 = 1' | tee -a /etc/sysctl.conf
+echo 0 | tee /proc/sys/net/ipv4/ip_forward
