4d94fefade9eca9cf03a98857dbde492bf2cbb13
Hackerfox8100 <40575728+Hackerfox8100@users.noreply.github.com>
2024-04-09 19:30:56 -0400

    added more bad packages

--- wildcard/linux/Ubuntu/removeBadPackages.sh
+++ wildcard/linux/Ubuntu/removeBadPackages.sh
@@ -5,7 +5,7 @@ if [ $UID != 0 ]; then
     exit
 fi
 # delete bad packages
-for i in arp-scan braa dirb hashcat dnswalk faraday-server donna spampd ophcrack tmux snap pinta knocker nbtscan pompem crunch netcat lynis xprobe john zenmap binwalk sl john-data medusa hydra dsniff netcat-openbsd netcat-traditional traceroute telnet wireshark aircrack-ng pyrit zeitgeist nmap yersinia deluge httpry p0f dos2unix kismet transmission sendmail tightvncserver finger xinetd cain minetest tor moon-buggy dovecot rsh-server aisleriot hping3 freeciv darkstat nis sqlmap libaa-bin gdb skipfish extremetuxracer ninvaders freesweep nsnake bsdgames
+for i in arp-scan braa dirb hashcat dnswalk faraday-server donna snort spampd ophcrack tmux snap pinta knocker nbtscan pompem crunch netcat lynis xprobe john zenmap binwalk sl john-data medusa hydra dsniff netcat-openbsd netcat-traditional traceroute telnet wireshark aircrack-ng pyrit zeitgeist nmap yersinia deluge httpry p0f dos2unix kismet transmission sendmail tightvncserver finger xinetd cain minetest tor moon-buggy dovecot rsh-server aisleriot hping3 freeciv darkstat nis sqlmap libaa-bin gdb skipfish extremetuxracer ninvaders freesweep nsnake bsdgames
 do
     #faster than apt purge for every package
     if dpkg-query -W $i; then
